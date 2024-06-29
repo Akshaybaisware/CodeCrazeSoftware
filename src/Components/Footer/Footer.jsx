423073ce7e530552ca170363e448126748510a0a
Ritikjain91 <jainritik829@gmail.com>
2024-06-29 20:17:27 +0530

    Updated code

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Footer.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faLinkedinIn, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone, faMapMarkerAlt, faLink } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
@@ -11,27 +14,26 @@ const Footer = () => {
           <button className="contact-button">Contact Us</button>
         </div>
         <div className="quick-links">
-          <h3>Quick Links</h3>
+          <h3><FontAwesomeIcon icon={faLink} /> Quick Links</h3>
           <ul>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#online-support">Online Support</a></li>
-            <li><a href="#business-solution">Business Solution</a></li>
-            <li><a href="#faqs">FAQs</a></li>
+            <li><a href="#services"><FontAwesomeIcon icon={faLink} /> Services</a></li>
+            <li><a href="#online-support"><FontAwesomeIcon icon={faLink} /> Online Support</a></li>
+            <li><a href="#business-solution"><FontAwesomeIcon icon={faLink} /> Business Solution</a></li>
+            <li><a href="#faqs"><FontAwesomeIcon icon={faLink} /> FAQs</a></li>
           </ul>
         </div>
         <div className="contact-info">
-          <h3>Contact Information</h3>
-          <p>Email: <a href="mailto:prafullanathile@gmail.com">softwarecodecraze@gmail
-          .com</a>,</p>
-          <p>Phone: <a href="tel:+919970850512">9503007527/9403452788</a></p>
-          <p>Nagpur 440003 Medical Square.</p>
+          <h3><FontAwesomeIcon icon={faMapMarkerAlt} /> Contact Information</h3>
+          <p><FontAwesomeIcon icon={faEnvelope} /> Email: <a href="mailto:softwarecodecraze@gmail.com">softwarecodecraze@gmail.com</a></p>
+          <p><FontAwesomeIcon icon={faPhone} /> Phone: <a href="tel:+919970850512">9503007527/9403452788</a></p>
+          <p><FontAwesomeIcon icon={faMapMarkerAlt} /> Nagpur 440003 Medical Square.</p>
         </div>
         <div className="map">
           <iframe
             title="Prevoyance IT Solutions Location"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14227.667213482834!2d79.0916063!3d21.1323985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c11dbd9bb3f1%3A0xc4b8a33d4e7e1a45!2sPrevoyance%20IT%20Solutions%20Private%20Limited!5e0!3m2!1sen!2sin!4v1673012462530!5m2!1sen!2sin"
-            width="300"
-            height="200"
+            width="400"
+            height="300"
             style={{ border: 0 }}
             allowFullScreen=""
             loading="lazy"
@@ -41,11 +43,11 @@ const Footer = () => {
       <div className="footer-bottom">
         <p>&copy; CodeCraze Software Solutions Pvt Ltd © 2023. All Rights Reserved.</p>
         <div className="social-links">
-          <a href="#"><i className="fab fa-facebook-f"></i></a>
-          <a href="#"><i className="fab fa-twitter"></i></a>
-          <a href="#"><i className="fab fa-linkedin-in"></i></a>
-          <a href="#"><i className="fab fa-youtube"></i></a>
-          <a href="#"><i className="fab fa-instagram"></i></a>
+          <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
+          <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+          <a href="#"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+          <a href="#"><FontAwesomeIcon icon={faYoutube} /></a>
+          <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
         </div>
       </div>
     </footer>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,8 +5,10 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Carousel from "react-bootstrap/Carousel";
-import ExampleCarouselImage from "../Carousel/ExampleCarouselImage.jsx";
-import ExampleCarouselImage1 from "../Carousel/ExampleCarouselImage1.jsx";
+import shipyaari from '../../Images/shipyaari.jpg';
+import airrnp from '../../Images/airrnp.jpg';
+import getmock from '../../Images/getmock.jpg';
+import parkers from '../../Images/parkres.jpg';
 
 import Picture1 from "../../Images/TehnologyImage1.jpeg";
 import Picture2 from "../../Images/TechnologyImage2.jpg";
@@ -58,7 +60,7 @@ function Home() {
   ];
 
   return (
-    <div className="p-0 m-0">
+    <div className="p-0 m-0 Home" >
       <Carousel>
         <Carousel.Item>
           <img
@@ -95,10 +97,10 @@ function Home() {
       </Carousel>
 
       <div className="Home__tiles">
-  <Row className="m-0 p-0  Home__tilesRow">
+  <Row className="m-0 p-0 Home__tilesRow">
     <Col xs={12} md={6} lg={4} className="m-0 p-0 order-md-1">
       <div style={{ position: 'relative' }}>
-        <Image src={Picture5} fluid  />
+        <Image src={Picture5} fluid style={{ height: '400px', objectFit: 'cover' }} />
         <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', textAlign: 'center' }}>
           <i className="bi fs-1 bi-pause-fill"></i>
           <h3 className="fw-bold fs-3">We design</h3>
@@ -112,11 +114,11 @@ function Home() {
     </Col>
     <Col xs={12} md={12} lg={4} className="m-0 p-0 order-md-3">
       <div className="text-center" style={{ position: 'relative' }}>
-        <Image src={Picture6} fluid className="custom-image-height" />
+        <Image src={Picture6} fluid style={{ height: '400px', objectFit: 'cover' }} className="" />
         <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', textAlign: 'center' }}>
           <i className="bi fs-1 bi-play"></i>
           <h3 className="fw-bold fs-2">We develop</h3>
-          <p className="fs-6 color-light  custom-p-tag ">
+          <p className="fs-6 color-light custom-p-tag">
             CodeCrazeSoftware Solutions, we are bestowed with dexterity to
             develop all your minutest application development specifications
             acutely & exact to the described requirements.
@@ -126,7 +128,7 @@ function Home() {
     </Col>
     <Col xs={12} md={6} lg={4} className="m-0 p-0 order-md-2">
       <div className="text-center" style={{ position: 'relative' }}>
-        <Image src={Picture7} fluid  />
+        <Image src={Picture7} fluid style={{ height: '400px', objectFit: 'cover' }} />
         <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', textAlign: 'center' }}>
           <i className="bi fs-1 bi-stop-fill"></i>
           <h3 className="fw-bold fs-2">We test it</h3>
@@ -144,60 +146,23 @@ function Home() {
 
 
 
-      <div className="BackgroundImage">
-        <Image src={BackgroundImage} fluid className="w-100" />
-        <Container className="text-overlay position-absolute top-50 start-50 text-center translate-middle">
-          <h1 className="fw-bold">We Work With.</h1>
-          <p className="fw-light">
-            Prevoyance Solutions is a team of professionals who are striving to
-            deliver the best IT solutions to the people we work with.
-          </p>
-        </Container>
-      </div>
+<div className="BackgroundImage">
+  <Image src={BackgroundImage} fluid className="w-100  Home__BackgroundImage" />
+  <Container className="text-overlay position-absolute top-0 start-50 text-center translate-middle">
+    <h1 className="fw-bold fs-1">We Work With.</h1>
+    <p className="fs-5 text-body-secondary">
+      CodeCrazeSoftware Solutions is a team of professionals who are striving to
+      deliver the best IT solutions to the people we work with.
+    </p>
+  </Container>
 
-      <Container className="Picture-Container">
-        <Row className="d-lg-none">
-          <Col>
-            <Carousel className="Carousel-Image">
-              <Carousel.Item>
-                <Image src={Picture8} className="d-block w-100" alt="Enterprise Solutions" />
-                <p className="fs-5 fw-bold">Enterprise Solutions</p>
-                <p className="text-black-50">
-                  We work with large enterprises as well as small & medium-sized
-                  enterprises. We transform the way small, medium & large
-                  enterprises conduct their business with customers / clients,
-                  vendors / suppliers, partners and employees in this digital
-                  age.
-                </p>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image src={Picture9} className="d-block w-100" alt="Agencies" />
-                <p className="fs-5 fw-bold">Agencies</p>
-                <p className="text-black-50">
-                  We work with consulting companies, independent software
-                  vendors (ISVs), software product development companies or
-                  agencies. We partner with agencies to work on a project
-                  end-to-end, or as an extended team to build their client's
-                  project successfully.
-                </p>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image src={Picture10} className="d-block w-100" alt="Startups" />
-                <p className="fs-5 fw-bold">Startups</p>
-                <p className="text-black-50">
-                  We worked with more than 100+ global tech startups for their
-                  web/cloud/mobile/IOT needs. We empower and support startups
-                  with our digital expertise & experience at each stage whether
-                  you are at Seed stage OR Expansion stage.
-                </p>
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
+  <Container className="Home-Picture-Container">
 
-        <Row className="d-none d-lg-flex">
-          <Col xs={12} md={4}>
-            <Image src={Picture8} thumbnail />
+    <Row className="d-md-none">
+      <Col>
+        <Carousel className="Home-Carousel-Image">
+          <Carousel.Item>
+            <Image src={Picture8} className="d-block w-100 Home-Image" alt="Enterprise Solutions" />
             <p className="fs-5 fw-bold">Enterprise Solutions</p>
             <p className="text-black-50">
               We work with large enterprises as well as small & medium-sized
@@ -205,72 +170,185 @@ function Home() {
               enterprises conduct their business with customers / clients,
               vendors / suppliers, partners and employees in this digital age.
             </p>
-            <Button variant="secondary" className="Paragraph mx-4 px-4 rounded-pill" size="lg">
-              More
-            </Button>
-          </Col>
-          <Col xs={12} md={4}>
-            <Image src={Picture9} thumbnail />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={Picture9} className="d-block w-100 Home-Image" alt="Agencies" />
             <p className="fs-5 fw-bold">Agencies</p>
             <p className="text-black-50">
-              We work with consulting companies, independent software vendors
-              (ISVs), software product development companies or agencies. We
-              partner with agencies to work on a project end-to-end, or as an
-              extended team to build their client's project successfully.
+              We work with consulting companies, independent software
+              vendors (ISVs), software product development companies or
+              agencies. We partner with agencies to work on a project
+              end-to-end, or as an extended team to build their client's
+              project successfully.
             </p>
-            <Button variant="secondary" className="Paragraph mx-4 px-4 rounded-pill" size="lg">
-              More
-            </Button>
-          </Col>
-          <Col xs={12} md={4}>
-            <Image src={Picture10} thumbnail />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={Picture10} className="d-block w-100 Home-Image" alt="Startups" />
             <p className="fs-5 fw-bold">Startups</p>
             <p className="text-black-50">
               We worked with more than 100+ global tech startups for their
-              web/cloud/mobile/IOT needs. We empower and support startups with
-              our digital expertise & experience at each stage whether you are
-              at Seed stage OR Expansion stage.
+              web/cloud/mobile/IOT needs. We empower and support startups
+              with our digital expertise & experience at each stage whether
+              you are at Seed stage OR Expansion stage.
             </p>
-            <Button variant="secondary" className="Paragraph mx-4 px-4 rounded-pill" size="lg">
-              More
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+          </Carousel.Item>
+        </Carousel>
+      </Col>
+    </Row>
+
+    
+    <Row className="d-none d-md-flex d-lg-none">
+      <Col xs={12} md={6}>
+        <Image src={Picture8} className="Home-Image img-fluid" />
+        <p className="fs-5 fw-bold">Enterprise Solutions</p>
+        <p className="text-black-50">
+          We work with large enterprises as well as small & medium-sized
+          enterprises. We transform the way small, medium & large
+          enterprises conduct their business with customers / clients,
+          vendors / suppliers, partners and employees in this digital age.
+        </p>
+        <Button variant="secondary" className="Home-Paragraph mx-4 px-4 rounded-pill" size="lg">
+          More
+        </Button>
+      </Col>
+      <Col xs={12} md={6}>
+        <Image src={Picture9} className="Home-Image img-fluid" />
+        <p className="fs-5 fw-bold">Agencies</p>
+        <p className="text-black-50">
+          We work with consulting companies, independent software vendors
+          (ISVs), software product development companies or agencies. We
+          partner with agencies to work on a project end-to-end, or as an
+          extended team to build their client's project successfully.
+        </p>
+        <Button variant="secondary" className="Home-Paragraph mx-4 px-4 rounded-pill" size="lg">
+          More
+        </Button>
+      </Col>
+    </Row>
+
+  
+    <Row className="d-none d-lg-flex">
+      <Col xs={12} md={4}>
+        <Image src={Picture8} className="Home-Image img-fluid" />
+        <p className="fs-5 fw-bold">Enterprise Solutions</p>
+        <p className="text-black-50">
+          We work with large enterprises as well as small & medium-sized
+          enterprises. We transform the way small, medium & large
+          enterprises conduct their business with customers / clients,
+          vendors / suppliers, partners and employees in this digital age.
+        </p>
+        <Button variant="secondary" className="Home-Paragraph mx-4 px-4 rounded-pill" size="lg">
+          More
+        </Button>
+      </Col>
+      <Col xs={12} md={4}>
+        <Image src={Picture9} className="Home-Image img-fluid" />
+        <p className="fs-5 fw-bold">Agencies</p>
+        <p className="text-black-50">
+          We work with consulting companies, independent software vendors
+          (ISVs), software product development companies or agencies. We
+          partner with agencies to work on a project end-to-end, or as an
+          extended team to build their client's project successfully.
+        </p>
+        <Button variant="secondary" className="Home-Paragraph mx-4 px-4 rounded-pill" size="lg">
+          More
+        </Button>
+      </Col>
+      <Col xs={12} md={4}>
+        <Image src={Picture10} className="Home-Image img-fluid" />
+        <p className="fs-5 fw-bold">Startups</p>
+        <p className="text-black-50">
+          We worked with more than 100+ global tech startups for their
+          web/cloud/mobile/IOT needs. We empower and support startups with
+          our digital expertise & experience at each stage whether you are
+          at Seed stage OR Expansion stage.
+        </p>
+        <Button variant="secondary" className="Home-Paragraph mx-4 px-4 rounded-pill" size="lg">
+          More
+        </Button>
+      </Col>
+    </Row>
+  </Container>
+</div>
+
 
       
-      <div className="Our_Work">
-        <Container className="content-container">
-          <h1 className="fs-bold">Our Work.</h1>
-          <p className="fs-lighter fs-4">
-            Yes, at Prevoyance, we're obsessed with quality. We rely on our
-            portfolio to do business and <br /> always ensure that each of our
-            web or mobile deliverable is unique and world class.
-          </p>
+      <div className="Home__carousel ">
+        <Container className="Home__Description text-center mb-5 ">
+        <p className="fs-1 fw-bold ">Our Work</p>
+        <p className=" fs-5" >Yes, at CodeCraze Software, we're obsessed with quality. We rely on our portfolio to do business and<br/> always ensure that each of our web or mobile deliverable is unique and world class.
+        </p>
         </Container>
+      <Carousel data-bs-theme="dark" interval={3000} className="Homecarousel__image"> 
+        <Carousel.Item>
+          <Row className="d-flex justify-content-center">
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+              <img className="d-block w-100" src={shipyaari} alt="Shipyaari" />
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 d-none d-sm-block">
+              <img className="d-block w-100" src={airrnp} alt="AirRNP" />
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 d-none d-md-block">
+              <img className="d-block w-100" src={getmock} alt="GetMock" />
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 d-none d-lg-block">
+              <img className="d-block w-100" src={parkers} alt="Parkers" />
+            </Col>
+          </Row>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Row className="d-flex justify-content-center">
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+              <img className="d-block w-100" src={airrnp} alt="AirRNP" />
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 d-none d-sm-block">
+              <img className="d-block w-100" src={getmock} alt="GetMock" />
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 d-none d-md-block">
+              <img className="d-block w-100" src={parkers} alt="Parkers" />
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 d-none d-lg-block">
+              <img className="d-block w-100" src={shipyaari} alt="Shipyaari" />
+            </Col>
+          </Row>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Row className="d-flex justify-content-center">
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+              <img className="d-block w-100" src={getmock} alt="GetMock" />
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 d-none d-sm-block">
+              <img className="d-block w-100" src={parkers} alt="Parkers" />
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 d-none d-md-block">
+              <img className="d-block w-100" src={shipyaari} alt="Shipyaari" />
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-3 d-none d-lg-block">
+              <img className="d-block w-100" src={airrnp} alt="AirRNP" />
+            </Col>
+          </Row>
+        </Carousel.Item>
+      </Carousel>
+    </div>
 
-        <Carousel className="bg-dark">
-    <Carousel.Item interval={5000}>
-      <ExampleCarouselImage text="First slide" />
-    </Carousel.Item>
-    <Carousel.Item interval={5000}>
-      <ExampleCarouselImage1 text="Second slide" />
-    </Carousel.Item>
-  </Carousel>
-      </div>
+
+
+
 
       <Container className="Home__approachesection">
-        <h2 className="text-center mb-5">Our business approach.
-        </h2>
-        <p> Every business is different and our approach towards clients makes us stand in crowd, making Prevoyance Solutions as their first choice for IT solutions.
+      
+        <p className="text-center fs-1 fw-bold Our__business ">Our business approach.
         </p>
-        <Row>
+        <p className="text-body-secondary mb-5 text-center "> Every business is different and our approach towards clients makes us stand in crowd, making CodeCrazeSoftware solution  as their first choice for IT solutions.
+        </p>
+    
+        <Row className="mx-5 text-center">
           {approaches.map((approach, index) => (
             <Col key={index} xs={12} md={6} lg={4} className="mb-4">
               <div className="approach-card p-3">
                 <img src={approach.icon} alt={approach.title} className="icon mb-3" />
                 <h3 className="fw-bold">{approach.title}</h3>
-                <p>{approach.description}</p>
+                <p className="text-body-secondary " >{approach.description}</p>
               </div>
             </Col>
           ))}
@@ -278,18 +356,18 @@ function Home() {
       </Container>
 
    
-    <Container fluid className="padding_fourth p-0">
-      <Row className="m-0">
-        <Col xl={6} lg={6} md={12} sm={12} xs={12} className="p-0">
+    <Container fluid className="padding_fourth ">
+      <Row >
+        <Col xl={6} lg={6} md={12} sm={12} xs={12} className="p-0 m-0">
           <Image src={SideImage} alt="Sixth Section" className="sixth_image w-100 h-100" />
         </Col>
         <Col xl={6} lg={6} md={12} sm={12} xs={12} className="p-0 text-white d-flex align-items-center">
           <div className="background_sixth p-5 w-100 h-100 d-flex flex-column justify-content-center">
-            <h2 className="heading_sixth fw-bold fs-1 mb-5">Engaging,<br/> purposeful, and<br/> creative<span className="extra">.</span></h2>
-            <p className="heading_sub_sixth mb-5">
+            <h2 className="heading_sixth fw-bold p-5 fs-2 mb-1">Engaging,<br/> purposeful, and<br/> creative<span className="extra">.</span></h2>
+            <p className="heading_sub_sixth mb-3 fs-5 p-5">
               Every business is unique and so are its needs. At CodeCrazeSoftware solution, we provide the best solutions for designing, development, and digital marketing.
             </p>
-            <Button variant="light" className="mt-4 w-50 rounded-pill btn_apply_sixth">
+            <Button variant="light" className="mt-2 rounded-pill btn_apply_sixth">
               Enquire Us
             </Button>
           </div>
